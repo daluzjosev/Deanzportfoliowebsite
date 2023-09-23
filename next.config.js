ee682cbbcb5f9b8a10869babe6ffc9e31720e6d5
fort <daluzjosev@gmail.com>
2023-09-23 16:26:59 +0800

    version 1.5

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/github-pages'
+    basePath: '/Deanzportfoliowebsite'
 }
 
 module.exports = nextConfig
