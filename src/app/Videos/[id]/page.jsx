0cda46fa5328bdaceff5836570111ffc8600b2c7
fort <daluzjosev@gmail.com>
2023-09-26 01:20:07 +0800

    version 8

--- src/app/Videos/[id]/page.jsx
+++ src/app/Videos/[id]/page.jsx
@@ -12,21 +12,7 @@ export default function page({ params }) {
     </>
   )
 }
-export async function getStaticProps({ params }) {
-  try {
-    const response = await fetch(`https://my-json-server.typicode.com/daluzjosev/deanzvideos/videos/${params.id}`);
-    if (!response.ok) {
-      throw new Error('Failed to fetch video data');
-    }
-    const data = await response.json();
-    console.log(data);
 
-    return { props: { video : data } };
-  } catch (error) {
-    console.error(error);
-    return { props: { video: null } }; // Set video prop to null to handle the error gracefully
-  }
-}
 
 export async function getStaticPaths() {
   const response = await fetch('https://my-json-server.typicode.com/daluzjosev/deanzvideos/videos');
