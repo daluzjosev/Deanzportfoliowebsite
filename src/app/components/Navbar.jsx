d6e9e29fadf41cd8b9d97b74b060d9b44f3a5dfc
fort <daluzjosev@gmail.com>
2023-10-21 14:32:59 +0800

    bug with logo fix

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -11,15 +11,19 @@ import Contactlist from "./Contactlist";
 export default function Navbar() {
   return (
     <nav>
+      
+        
         <div className='flex'>
         <Link href={"/"}> <h1 className='text-5xl nav-title'>Dean Ver Tinio</h1></Link>
         
         </div>
-        
+        <div  className='flex pl-3'>
+        <Link href={"/"}><Image className='nav-logo' src="/Deanzportfoliowebsite/images/android-chrome-512x512.png" width={60} height={60} alt="icon" /></Link>
+
+      </div>
         <div className="flex">
           
 
-          <Link href={"/"}><Image className='nav-logo pl-2' src="/Deanzportfoliowebsite/images/android-chrome-512x512.png" width={60} height={60} alt="icon" /></Link>
           <Navlinks />
 
           <ul className='flex space-x-2 mt-2 card-title'>
